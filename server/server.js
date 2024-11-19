c0961d6fb71cd28308f12f7119821e3b2209637f
Yashpawar-2005 <yashpawar12312@gmail.com>
2024-11-19 22:23:59 +0530

    socket still not working as expected  added the togglers and stuff

--- server/server.js
+++ server/server.js
@@ -1,33 +1,74 @@
-import cookieParser from "cookie-parser";
-import express, { json } from "express";
-const app=express();
-import authroutes from './routes/authroutes.route.js'
-import roomroutes from './routes/roomroutes.route.js'
-import connectDB from "./db/connect.db.js";
-import cors from 'cors'
-import dotev from 'dotenv'
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { createServer } from 'http'; 
+import { Server } from 'socket.io';  
+import dotenv from 'dotenv';
+import authroutes from './routes/authroutes.route.js';
+import roomroutes from './routes/roomroutes.route.js';
+import connectDB from './db/connect.db.js';
 
-app.use(json());
-const port =process.env.VITE_PORT||4000;
-app.use(express.urlencoded({extended:true}))
+dotenv.config();
+
+const app = express();
+
+
+const port = process.env.VITE_PORT || 4000;
+
+
+app.use(express.json());
 app.use(cookieParser());
-dotev.config();
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow requests from this origin
-    credentials: true, // Allow cookies to be sent
-  }));
-  
-
-
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 connectDB();
-app.use("/api/auth",authroutes)
-app.use("/api/room",roomroutes)
-app.get('/', (req, res) => {
-    console.log('server hitthin')
-    res.send("server is ready")
 
+app.use('/api/auth', authroutes);
+app.use('/api/room', roomroutes);
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST'],
+    credentials: true,  
+  }
+});
+io.on('connection', (socket) => {
+  let editorValue=""
+  
+  
+  socket.on('comm',({usernamee,typee})=>{
+   
+    socket.broadcast.emit('comm',({usernamee,typee}))
 })
+  
+socket.on('broadcast', (updatedEditor) => {
+  editorValue += updatedEditor;
+  socket.emit('getvalue', editorValue);
+});
 
-console.log(port)
-app.listen(port);
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+    socket.broadcast.emit('message', `You said: ${data}`);
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    socket.broadcast.emit('user disconnected')
+  });
+});
+
+
+app.get('/', (req, res) => {
+  res.send("Server is ready");
+});
+
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
